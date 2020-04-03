d9f994ddd65163a56dd5920a688465ba9c5f7568
Ryan Freeman <N00173767@student.iadt.ie>
2020-04-03 01:11:01 +0100

    Production build

--- resources/js/api/index.js
+++ resources/js/api/index.js
@@ -6,11 +6,14 @@ const UNAUTHORISED = 401;
 let baseUrl,
     env = process.env.NODE_ENV;
 
-if (env === 'development') {
-    baseUrl = 'http://localhost:8000/api/'
-} else if (env === 'production') {
-    baseUrl = 'http://ancient-stream-40225.herokuapp.com/api/'
-}
+// if (env === 'development') {
+//     baseUrl = 'http://localhost:8000/api/'
+// } else {
+//     baseUrl = 'http://ancient-stream-40225.herokuapp.com/api/'
+// }
+
+baseUrl = 'http://ancient-stream-40225.herokuapp.com/api/';
+
 
 // define and export an axios client
 export const api = axios.create({
