0f018b6fb7cea80e908ed9aa15362f58094d564c
Ryan Freeman <N00173767@student.iadt.ie>
2020-04-05 19:48:46 +0100

    Added enrolment count to lecturers

--- resources/js/components/Lecturer.vue
+++ resources/js/components/Lecturer.vue
@@ -27,6 +27,9 @@
         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
             {{ lecturer.phone }}
         </td>
+        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 font-medium text-gray-900">
+            {{ lecturer.enrolments.length }}
+        </td>
     </router-link>
 </template>
 
--- resources/js/views/lecturers/Lecturer.vue
+++ resources/js/views/lecturers/Lecturer.vue
@@ -71,6 +71,14 @@
                                         {{ lecturer.phone }}
                                     </dd>
                                 </div>
+                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm leading-5 font-medium text-gray-500">
+                                        Enrolments
+                                    </dt>
+                                    <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {{ lecturer.enrolments.length }}
+                                    </dd>
+                                </div>
                             </dl>
                         </div>
                     </div>
